c43599598ad6085c377fb4343e2d19169f36303c
pisething <piseth.ing2022@gmail.com>
2025-08-24 19:47:49 +0700

    add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SectionComponent } from './components/section/section.component';
+import { PropertyDetailsComponent } from './components/property-details/property-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'properties', pathMatch: 'full' },
+  { path: 'properties', component: SectionComponent },            // list page
+  { path: 'properties/:id', component: PropertyDetailsComponent },   // details page (no resolver variant)
+  // If you want resolver-based prefetch:
+  // { path: 'properties/:id', component: PropertyDetailsPageComponent, resolve: { room: roomResolver } },
+  { path: '**', redirectTo: 'properties' }
+];
