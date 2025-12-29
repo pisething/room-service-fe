6622692bf700fd07062417fc02c1f1008d12c12a
pisething <112937826+pisething@users.noreply.github.com>
2025-12-29 21:03:53 +0700

    update menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FavoritesStore } from '../../services/favorite.store';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './title.component.css'
 })
 export class TitleComponent {
-
+title = input.required<string>();
+  subtitle = input<string>();
 }
